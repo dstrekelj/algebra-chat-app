8683b4a82619537ca4338e4a207b1bc0e4f9fab2
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-07-14 20:39:23 +0200

    Add Icon component

---
+++ src/components/Icon/Icon.component.jsx
@@ -0,0 +1,10 @@
+import "./Icon.styles.scss";
+
+import { ReactComponent as IconSend } from "../../assets/ic_baseline-send.svg";
+
+export function Icon(props) {
+  switch (props.type) {
+    case 'send': return <IconSend className="icon" />;
+    default: return null;
+  }
+}
