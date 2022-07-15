d08ccd52e3f4ebe2e7f44d0190b140057f32399e
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-07-15 18:35:27 +0200

    Add input form field component

---
+++ src/components/InputFormField/InputFormField.component.jsx
@@ -0,0 +1,16 @@
+import "./InputFormField.styles.scss";
+
+import { InputElement } from "../InputElement";
+
+export function InputFormField(props) {
+  return (
+    <div className="input-form-field">
+      <label className="input-form-field__label">
+        Display name
+      </label>
+      <div className="input-form-field__element">
+        <InputElement />
+      </div>
+    </div>
+  );
+}
