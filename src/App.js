811a703a5af802507e3d62171b075d7adee2c1d7
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-07-15 20:38:39 +0200

    Add avatar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Avatar } from "./components/Avatar";
 import { MessageText } from "./components/MessageText";
 
 function App() {
-  return <MessageText displayName="Domagoj" time="20:13">
-    Ready for the trip? I'm so excited I couldn't sleep!
-  </MessageText>;
+  return <div>
+    <Avatar text="DOM" backgroundColor="lime" />
+    <MessageText displayName="Domagoj Štrekelj" time="20:13">
+      Ready for the trip? I'm so excited I couldn't sleep!
+    </MessageText>
+  </div>
 }
 
 export default App;
