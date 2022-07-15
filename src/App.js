0d519a785d2ef44710f0b449a6aba3bf520b4815
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-07-15 20:02:02 +0200

    Add message form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { MessageForm } from "./components/MessageForm";
 import { JoinPage } from "./pages/JoinPage";
 
 function App() {
-  return <JoinPage />
+  return <MessageForm />;
 }
 
 export default App;
